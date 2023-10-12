2a8c101c75ccccd379170706da4bdd40d8cff8b3
Alanna <phan.alanna@gmail.com>
2023-10-12 16:40:39 -0400

    implemented form validation for empty text or text >140 characters

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-  loadTweets();
   submitTweet();
-  renderTweets(data);
+  loadTweets();
 });
 
 // intakes tweet data parameter and uses a template to return completed tweet element
@@ -18,7 +17,7 @@ const createTweetElement = function (tweetObject) {
     <p class="tweet-content">${tweetObject.content.text}</p>
     <hr>
     <footer>
-      <p class="date">${tweetObject.created_at}</p>
+      <p class="date">${timeago.format(tweetObject.created_at)}</p>
       <div class="tweet-icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -42,21 +41,34 @@ const renderTweets = function (tweets) {
   }
 };
 
-// prevents default of form submission and uses AJAX to send serialized data to server
+// serializes input data and sends to server
 const submitTweet = function () {
   $("form").on("submit", function (event) {
     event.preventDefault();
 
-    // serializes form input
+    // textarea input length used for form validation
+    const textAreaLength = $("form").find("textarea").val().length;
+    if (textAreaLength > 140) {
+      event.preventDefault();
+      alert("You are over the character limit! :(");
+      return;
+    }
+
+    if (textAreaLength === 0) {
+      event.preventDefault();
+      alert("Please write something before submitting!");
+      return;
+    }
+
+    // if all is well, serializes form input
     const data = $("form").serialize();
 
-    // sends serialized data to server
+    // and then sends serialized data to server
     $.post("/tweets", data);
-    console.log(data);
   });
 };
 
-// fetches array of tweets from /tweet as a JSON
+// fetches array of tweets from /tweet and calls renderTweets with that data
 const loadTweets = function () {
   $.get("/tweets", function (data) {
     renderTweets(data);
