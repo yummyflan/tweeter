d9bd7f9a9dddcc6cb9c3d8f989c75ad00b08ef65
Alanna <phan.alanna@gmail.com>
2023-10-12 01:57:36 -0400

    implemented functional counter for form when typing input

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  console.log("hello world");
+
+  $('textarea').on('input', function() {
+    let counter = $('textarea').parent().find("output").val(140 - (this.value.length));
+  });
+
+ 
+});
