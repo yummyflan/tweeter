6420733796d502b4052591f58285823fc0193a53
Alanna <phan.alanna@gmail.com>
2023-10-12 06:39:09 -0400

    removed hardcoded tweet from HTML and implemented dynamic tweet function in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,48 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  user: {
+    name: "Newton",
+    avatars: "https://i.imgur.com/73hZDYK.png",
+    handle: "@SirIsaac",
+  },
+  content: {
+    text: "If I have seen further it is by standing on the shoulders of giants",
+  },
+  created_at: 1461116232227,
+};
+
+$(document).ready(function() {
+  const $tweet = createTweetElement(tweetData);
+  
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+})
+
+const createTweetElement = function (tweetObject) {
+  const $tweet = $(`
+  <article class="tweet">
+    <header>
+      <div class="avatar-name">
+        <img src="${tweetObject.user.avatars}"/>
+        <p>${tweetObject.user.name}</p>
+      </div>
+      <p class="handle">${tweetObject.user.handle}</p>
+    </header>
+    <p class="tweet-content">${tweetObject.content.text}</p>
+    <hr>
+    <footer>
+      <p class="date">${tweetObject.created_at}</p>
+      <div class="tweet-icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `);
+
+  return $tweet;
+};
 
