b1116d7abc4eb45333dbc0b74d6a01c4970cdc2b
Alanna <phan.alanna@gmail.com>
2023-10-12 01:59:16 -0400

    formatted code in composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
 $(document).ready(() => {
   console.log("hello world");
 
-  $('textarea').on('input', function() {
-    let counter = $('textarea').parent().find("output").val(140 - (this.value.length));
+  $("textarea").on("input", function () {
+    let counter = $("textarea")
+      .parent()
+      .find("output")
+      .val(140 - this.value.length);
   });
-
- 
 });
