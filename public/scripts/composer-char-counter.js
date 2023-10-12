e5865193baa92adadf96516f2ba8edd115889fa3
Alanna <phan.alanna@gmail.com>
2023-10-12 03:22:32 -0400

    added counter font color change to red when negative value

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,13 @@ $(document).ready(() => {
   console.log("hello world");
 
   $("textarea").on("input", function () {
-    let counter = $("textarea")
-      .parent()
-      .find("output")
-      .val(140 - this.value.length);
+    let updateCounter = $("textarea").parent().find("output").val(140 - this.value.length);
+    let counterNumber = $("textarea").parent().find("output");
+    
+      if (counterNumber.val() < 0) {
+        counterNumber.addClass("counter-negative");
+      } else {
+        counterNumber.removeClass("counter-negative");
+      }
   });
 });
