7932ea463c50e2c55981ff492ea65d6b4ed09204
Alanna <phan.alanna@gmail.com>
2023-10-12 06:55:55 -0400

    added renderTweets function to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,33 +1,51 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac",
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants",
-  },
-  created_at: 1461116232227,
-};
-
 $(document).ready(function() {
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+  renderTweets(data);
 })
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+// loops through array of tweets
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $("#tweets-container").append($tweet);
+  }
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+}
 
 const createTweetElement = function (tweetObject) {
   const $tweet = $(`
   <article class="tweet">
-    <header>
-      <div class="avatar-name">
-        <img src="${tweetObject.user.avatars}"/>
-        <p>${tweetObject.user.name}</p>
-      </div>
+  <header>
+  <div class="avatar-name">
+  <img src="${tweetObject.user.avatars}"/>
+  <p>${tweetObject.user.name}</p>
+  </div>
       <p class="handle">${tweetObject.user.handle}</p>
     </header>
     <p class="tweet-content">${tweetObject.content.text}</p>
@@ -45,4 +63,6 @@ const createTweetElement = function (tweetObject) {
 
   return $tweet;
 };
+
+
 
